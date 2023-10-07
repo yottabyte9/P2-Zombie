dc4caed31199554d90f9de8c4f174495136b8456
yottabyte9 <andrewcn@umich.edu>
2023-10-07 13:09:19 -0400

    Random done not tested

--- main.cpp
+++ main.cpp
@@ -2,60 +2,152 @@
 #include <string>
 #include <math.h>
 #include <algorithm>
+#include <queue>
+#include <getopt.h>
+#include <fstream>
+#include <map>
+#include "P2random.h"
+
 using namespace std;
 
-int quiver;
+uint32_t quiver;
+int nround = 0;
+int rand_seed;
+int rand_distance, rand_speed, rand_health;
+bool keep_going = true;
 
+int rand_num;
+int named_num;
 
-class Zombie{
+struct Zombie{
     public:
         string name;
         uint32_t distance;
         uint32_t speed;
         uint32_t health;
-        int rounds;
+        uint32_t round;
+        Zombie(string n, uint32_t d, uint32_t s, uint32_t h, uint32_t r){
+            name = n;
+            distance = d;
+            speed = s;
+            health = h;
+            round = r;
+        }
 };
 
-bool operator< (Zombie& z1, Zombie& z2){ //return true if z1 has higher priority (lower eta)
-    if(eta(z1) < eta(z2)){
-        return true;
-    }
-    else if(eta(z2) < eta(z1)){
-        return false;
-    }
-    else{ //same eta
-        if(z1.health < z2.health){
+int eta(const Zombie* zombie);
+void end_sequence();
+
+struct Comparator{
+    bool operator()(const Zombie* z2, const Zombie* z1) const{ //return true if z1 has higher priority (lower eta)
+        if(eta(z1) < eta(z2)){
             return true;
         }
-        else if(z1.health > z2.health){
+        else if(eta(z2) < eta(z1)){
             return false;
         }
+        else{ //same eta
+            if((*z1).health < (*z2).health){
+                return true;
+            }
+            else if((*z1).health > (*z2).health){
+                return false;
+            }
+        }
+        return (*z1).name < (*z2).name;
     }
-    return z1.name < z2.name;
+};
+
+priority_queue<Zombie*, vector<Zombie*>, Comparator> pq; 
+deque<Zombie*> master;
+deque<Zombie*> dead;
+
+
+int eta(const Zombie* zombie){
+    return (*zombie).distance/(*zombie).speed;
 }
 
-void updateDist(Zombie &zombie){
-    zombie.distance = min(0, int(zombie.distance-zombie.speed));
-    if(zombie.distance == 0){
-        //end game;
-    }
-}
-
-int eta(Zombie &zombie){
-    return zombie.distance/zombie.speed;
-}
-
-void shoot(Zombie &zombie, int current_quiver){
-    if(zombie.health > current_quiver){
-        zombie.health -= current_quiver;
+int shoot(Zombie *zombie, uint32_t current_quiver){
+    cout << "shooting: " << zombie->name << "\n";
+    if(zombie->health > current_quiver){
+        zombie->health -= current_quiver;
+        current_quiver = 0;
     }
     else{
-        zombie.health = 0;
+        zombie->health = 0;
         //remove zombie
-        current_quiver -= zombie.health;
+        cout << "killed: " << zombie->name << "\n";
+        current_quiver -= zombie->health;
+        dead.push_back(zombie);
+        pq.pop();
+    }
+    return current_quiver;
+}
+
+void round(int round){
+    int current_quiver = quiver;
+    cout << "current round: " << round << "\n";
+
+    while(current_quiver > 0 && !pq.empty()){
+        current_quiver = shoot(pq.top(), current_quiver);
     }
 }
 
+void move_forward(){ 
+    for(auto zombie: master){
+        if(zombie->health == 0){
+            return;
+        }
+        cout << "moving: " << zombie->name << "\n";
+        zombie->distance = max(0, int(zombie->distance-zombie->speed));
+        if(zombie->distance == 0){
+            keep_going = false;
+            cout << "killed by: " << zombie->name << "\n";
+        }
+    }
+}
+
+void cl(int argc, char** argv){
+    //find round
+    //put zombies into pq
+    //start round
+    //shoot zombies
+
+    int gotopt;
+    int option_index = 0;
+    option long_opts[]={
+        {"verbose", no_argument, nullptr, 'v'},
+        {"statistics", required_argument, nullptr, 's'},
+        {"median", no_argument, nullptr, 'm'},
+        {"help", no_argument, nullptr, 'h'},
+        {0, 0, 0, 0}
+    };
+
+    uint32_t num;
+    bool s, m, v = false;
+
+    while((gotopt = getopt_long(argc, argv, "vs:mh", long_opts, &option_index)) != -1){
+        switch(gotopt){
+            case 'h':
+                cout << "help";
+                exit(0);
+            case 's':
+                s = true;
+                num = atoi(optarg);
+                break;
+            case 'm':
+                m = true;
+                break;
+            case 'v':
+                v = true;
+                break;
+            default:
+                cout << "default error\n";
+                exit(1);
+        }
+    }
+    cout << num << s << m << v << "\n";
+}
 
 void fill_initial(){
     string junk;
@@ -66,76 +158,105 @@ void fill_initial(){
             cin >> line;
         }
     }
+    cin >> line; //quiver-capacity:
     cin >> line;
-    while(line != "---"){
-        cout << line << "\n";
-        cin >> line;
-    }
+    quiver = stoi(line);
+    cin >> line; // random-seed:
+    cin >> line;
+    rand_seed = stoi(line);
+    cin >> line; //max-rand-distance:
+    cin >> line;
+    rand_distance = stoi(line);
+    cin >> line; //max-rand-speed:
+    cin >> line;
+    rand_speed = stoi(line);
+    cin >> line; //max-rand-health:
+    cin >> line;
+    rand_health = stoi(line);
+    cout << quiver << ", " << rand_seed << ", " << rand_distance << ", " << rand_speed << ", " << rand_health << "\n";
+    P2random::initialize(rand_seed,rand_distance,rand_speed,rand_health);
 } 
 
-void fill_round(){
+int fill_round(){
     string line;
+    uint32_t round_temp = 1;
+
+    string name;
+    uint32_t distance;
+    uint32_t speed;
+    uint32_t health;
+
+    uint32_t random_zombies = 0;
+    uint32_t named_zombies = 0;
     
+
     cin >> line;
-    while(line != "---"){
-        if(line[0] == '\n'){
-            cout << "\n";
+    if(empty(line)){
+        keep_going = false;
+        return 0;
+    } 
+    cin >> line; //round
+    cin >> line;
+
+    if(stoi(line) > nround+1){
+        while(keep_going && stoi(line) > nround+1){
+            nround++;
+            round(nround);
+            move_forward();
         }
-        if(isdigit(line[0])){
-            cout << line << " ";
-            cin >> line;
-        }
-        else{
-            cin >> line;
+        if(!keep_going){
+            end_sequence();
         }
     }
+
+    round_temp = stoi(line); 
+    nround = round_temp;
+
+    cin >> line >> line; //randomzombies
+    random_zombies = stoi(line);
+    cin >> line >> line; //namedzombies
+    named_zombies = stoi(line);
+    //cout << round << "\n" << random_zombies << "\n" << named_zombies << "\n";
+    for(uint32_t i=0; i<random_zombies; i++){
+        string name  = P2random::getNextZombieName();
+        uint32_t distance = P2random::getNextZombieDistance();
+        uint32_t speed    = P2random::getNextZombieSpeed();
+        uint32_t health   = P2random::getNextZombieHealth();
+        master.push_back(new Zombie(name, distance, speed, health, round_temp));
+        pq.push(master.back());
+    }
+    for(uint32_t i=0; i<named_zombies; i++){
+        cin >> name;
+        cin >> line >> line;
+        distance = stoi(line);
+        cin >> line >> line;
+        speed = stoi(line);
+        cin >> line >> line;
+        health = stoi(line);
+        //cout << "name: " << name << " distance: " << distance << " speed: " << speed << " health: " << health << "\n";
+        master.push_back(new Zombie(name, distance, speed, health, round_temp));
+        pq.push(master.back());
+    }
+    return round_temp;
 }
 
+void end_sequence(){
+    cout << "end sequence \n";
+}
 
-main(){
+int main(int argc, char **argv){
     ios_base::sync_with_stdio(false);
+
     cl(argc, argv);
     fill_initial();
-    fill_round();
-}
-
-
-ordered_map<int, int> median;
-int mapsize = 0;
-if(destroyed){
-    median[current_round - zombie.round_created] ++;
-    mapsize ++;
-}
-if(round end){
-    int counter = 0;
-    bool flag = false;
-    int prev_store;
-
-    if(mapsize == 0){
-        return;
-    }
-
-    if(mapsize == 1){
-        for(auto i: median){
-            return i.first;
-        }
-    }
-
-    for(auto i: median){
-        
-        if(flag){
-            return (prev_store + i.first)/ 2;
+    while(keep_going){
+        move_forward();
+        if(!keep_going){
+            end_sequence();
             break;
         }
-        if(counter < mapsize/2 && counter + i.second > mapsize/2){ 
-            return i.first;
-        }
-        else if(counter + i.second == mapsize/2){ 
-            prev_store = i.first;
-            flag = true;
-        }
-        else{
-            counter += i.second;
-        }
+        fill_round();
+        round(nround);
     }
-}
+    
+};
